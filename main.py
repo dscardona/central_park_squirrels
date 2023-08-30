51c81c3756469beb299ec578221253288a940127
Diana Cardona <dscardona@gmail.com>
2023-08-30 17:44:33 -0400

    Edit todos

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas
 # Read csv file
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# Get Primary Fur Color column data
+color = data["Primary Fur Color"]
+color_list = color.to_list()
+
 # Find number of gray squirrels in column Primary Fur Color
 # Find number of black squirrels in column Primary Fur Color
 # Find number of cinnamon squirrels in column Primary Fur Color
-#Create dictionary with extracted data
-#Create new CSV file
+# Create dictionary with extracted data
+# Create dataframe from dictionary
+# Create new CSV file
