6e85e451c2329c36b64d92e18096f3966eaf9f44
Diana Cardona <dscardona@gmail.com>
2023-08-30 17:33:39 -0400

    Create main.py with ToDos

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas
+# Read csv file
+# Find number of gray squirrels in column Primary Fur Color
+# Find number of black squirrels in column Primary Fur Color
+# Find number of cinnamon squirrels in column Primary Fur Color
+#Create dictionary with extracted data
+#Create new CSV file
